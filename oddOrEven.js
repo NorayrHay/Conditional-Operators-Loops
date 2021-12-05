59ca23fb66afa0916a4dab1b4da37c6029881520
Norayr Hayrikyan <hayrikyannoro@gmail.com>
2021-12-06 02:28:32 +0400

    feat: my added file and oddOrEven task

---
+++ oddOrEven.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const arrOddEven = [125, 35, 20];
+
+for (let i = 0; i < arrOddEven.length; i++) {
+  if (arrOddEven[i] % 2 === 0) {
+    console.log('even');
+  }
+  else {
+    console.log('odd');
+  }
+}
