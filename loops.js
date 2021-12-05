0a4156a770b1a35fde745601b340655007a0c63b
Norayr Hayrikyan <hayrikyannoro@gmail.com>
2021-12-06 02:20:53 +0400

    feat: added loops.js

---
+++ loops.js
@@ -0,0 +1,18 @@
+// 'use strict'
+
+// const a = [1, 2, 4, 5];
+
+// let i = 0;
+// while (i < a.length) {
+
+//   console.log("while", i, a[i]);
+//   i++;
+// }
+
+// let i = 0;
+
+// do{
+//     console.log("do while", i, a[i]);
+//     i++;
+// }
+// while(i < a.length);
